9a6e1f39fdda079798ecb71be198e3b8282f1502
JorgenOlofssonSWE <jorgen.l.olofsson@gmail.com>
2023-03-19 22:33:37 +0100

    Adding serializers

---
+++ littlelemon/restaurant/serializers.py
@@ -0,0 +1,21 @@
+from rest_framework.serializers import ModelSerializer
+from django.contrib.auth.models import User
+from .models import Booking,MenuItem
+
+
+
+class MenuItemSerializer(ModelSerializer):
+    class Meta:
+        model = MenuItem
+        fields = ['title', 'price', 'inventory']
+
+
+class BookingSerializer(ModelSerializer):
+    class Meta:
+        model = Booking
+        fields = ['name', 'no_of_guests', 'bookingdate']
+
+class UserSerializer(ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['url', 'username', 'email', 'groups']
--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,7 +1,29 @@
 from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView,RetrieveUpdateAPIView,DestroyAPIView
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.decorators import api_view
+from rest_framework import permissions
+from .models import Booking,MenuItem
+from .serializers import MenuItemSerializer,BookingSerializer
 
 # Create your views here.
 
 def index(request):
     print ('in views indec')
     return render(request, 'index.html', {})
+
+class BookingViewSet(ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+    permission_classes = [permissions.IsAuthenticated] 
+
+class MenuItemsView(ListCreateAPIView):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
+
+class SingleMenuItemView(RetrieveUpdateAPIView,DestroyAPIView):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
+ 
+
+
